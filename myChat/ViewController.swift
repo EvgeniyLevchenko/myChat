3056e6defb80447e205e34a4d951f36b38aadd81
EvgeniyLevchenko <evgeniy.levchnko@gmail.com>
2022-08-11 16:02:46 +0300

    Initial Commit

---
+++ myChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myChat
+//
+//  Created by QwertY on 11.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
